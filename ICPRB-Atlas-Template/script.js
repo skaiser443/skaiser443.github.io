ca7dc37bebfd6942e28cfa731278dd0345f0fd66
Scott Kaiser <scottkaiser87@gmail.com>
2016-10-20 10:46:18 -0400

    updates
    latest version of draft template

--- ICPRB-Atlas-Template/script.js
+++ ICPRB-Atlas-Template/script.js
@@ -1,18 +1,18 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2thaXNlcmljcHJiIiwiYSI6ImNpa2U3cGN1dDAwMnl1cm0yMW94bWNxbDEifQ.pEG_X7fqCAowSN8Xr6rX8g';
+var bound = new mapboxgl.LngLatBounds(
+    new mapboxgl.LngLat(-81.457, 36.945),
+	new mapboxgl.LngLat(-72.49, 41.17)
+);
 var map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/skaisericprb/citvqu6qb002p2jo1ig5hnvtk',
 	center: [-77.975, 39.077],
-	setMaxBounds: [
-	  [-81.373, 41.554],
-	  [-74.763, 41.554],
-	  [-81.373, 36.282],
-	  [-74.763, 36.282]
-	],
+	maxBounds: bound,
 	zoom: 7.45,
 	attributionControl: {
 		position: 'bottom-right'
 	},
+	minZoom: [7.0],
 });
 
 map.addControl(new mapboxgl.Geocoder({position: 'top-right'}));
