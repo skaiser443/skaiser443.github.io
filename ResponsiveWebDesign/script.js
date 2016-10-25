f04a5c5834dd9c309fd807a255fde215521f69b4
Scott Kaiser <scottkaiser87@gmail.com>
2016-10-25 11:25:30 -0400

    Add files via upload

---
+++ ResponsiveWebDesign/script.js
@@ -0,0 +1,19 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2thaXNlcmljcHJiIiwiYSI6ImNpa2U3cGN1dDAwMnl1cm0yMW94bWNxbDEifQ.pEG_X7fqCAowSN8Xr6rX8g';
+var bound = new mapboxgl.LngLatBounds(
+    new mapboxgl.LngLat(-81.457, 36.945),
+	new mapboxgl.LngLat(-72.49, 41.17)
+);
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/skaisericprb/citvqu6qb002p2jo1ig5hnvtk',
+	center: [-77.975, 39.077],
+	maxBounds: bound,
+	zoom: 7.45,
+	attributionControl: {
+		position: 'bottom-right'
+	},
+	minZoom: [7.0],
+});
+
+map.addControl(new mapboxgl.Geocoder({position: 'top-right'}));
+map.addControl(new mapboxgl.Navigation({position: 'top-left'}));
